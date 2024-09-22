02fe328c002cd847a10d6917df7336414b542e7d
Hritik <hritikkumar.rd@gmail.com>
2024-09-23 00:29:15 +0530

    initialize the repository with working code sample

---
+++ src/main/java/com/application/ecommerce/database/MongoClient.java
@@ -0,0 +1,5 @@
+package com.application.ecommerce.database;
+
+public class MongoClient {
+
+}
---
+++ src/main/java/com/application/ecommerce/service/UserService.java
@@ -0,0 +1,27 @@
+package com.application.ecommerce.service;
+
+import com.application.ecommerce.modals.User;
+import com.application.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void saveUser(User user) {
+        repository.save(user);
+    }
+    public User getUser(String id) {
+        return repository.findById(id).orElse(null);
+    }
+    public List<User> getAllUsers(){
+        return repository.findAll();
+    }
+    public void deleteUser(String id) {
+        repository.deleteById(id);
+    }
+}
